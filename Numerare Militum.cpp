b216be1f27907fcaf96056c0ca24f5f49a402900
Rohit Gupta <49038290+Rohit2021@users.noreply.github.com>
2021-08-06 00:28:13 -0400

    Add files via upload

--- Numerare Militum.cpp
+++ Numerare Militum.cpp
@@ -15,14 +15,15 @@ int main()
     int Missing = 0;
     int Captured = 0;
     int Discharged = 0;
-    int Retired = 0;
 
     for (; ; ) {
         int input = 0;
+        int input2 = 0;
         system("CLS");
         cout << "Numerare Militum\n";
         cout << "Main Menu (Enter '#' for each option)\n";
-        cout << "\n'1' To View Personnel\n\n";
+        cout << "\n'1' To View Personnel Records\n";
+        cout << "'2' To Update Personnel Records\n\n";
         cin >> input;
 
         if (input == 1){
@@ -34,9 +35,44 @@ int main()
             cout << "'4' To View Deceased Personnel\n";
             cout << "'5' To View Missing Personnel\n";
             cout << "'6' To View Captured Personnel\n";
-            cout << "'7' To View Discharged Personnel\n";
-            cout << "'8' To View Retired Personnel\n";
-            cin >> input;
+            cout << "'7' To View Discharged Personnel\n\n";
+        
+            cin >> input2;
+            if (input2 == 1) {
+                cout << "Total Personnel - " << Total_Personnel;
+                cout << "\n(Enter any '#' to return to Main Menu)\n\n";
+                cin >> input;
+            }
+            else if (input2 == 2) {
+                cout << "Total Active Duty Personnel - " << Active_Duty;
+                cout << "\n(Enter any '#' to return to Main Menu)\n\n";
+                cin >> input;
+            }
+            else if (input2 == 3) {
+                cout << "Total Injured Personnel - " << Injured;
+                cout << "\n(Enter any '#' to return to Main Menu)\n\n";
+                cin >> input;
+            }
+            else if (input2 == 4) {
+                cout << "Total Deceased Personnel - " << Deceased;
+                cout << "\n(Enter any '#' to return to Main Menu)\n\n";
+                cin >> input;
+            }
+            else if (input2 == 5) {
+                cout << "Total Missing Personnel - " << Missing;
+                cout << "\n(Enter any '#' to return to Main Menu)\n\n";
+                cin >> input;
+            }
+            else if (input2 == 6) {
+                cout << "Total Captured Personnel - " << Captured;
+                cout << "\n(Enter any '#' to return to Main Menu)\n\n";
+                cin >> input;
+            }
+            else if (input2 == 7) {
+                cout << "Total Discharged Personnel - " << Discharged;
+                cout << "\n(Enter any '#' to return to Main Menu)\n\n";
+                cin >> input;
+            }
         }
     }
 }
